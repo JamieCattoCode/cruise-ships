72d6178562ada719439118d211e2e57b12ee003b
Jamie Catto <jamietcatto@gmail.com>
2022-10-21 16:38:49 +0200

    Added first tests for constructor method - checking it returns an Object instance with properties 'passengers' and 'startingPoint' of correct data types.

--- __tests__/cruise-ship.test.js
+++ __tests__/cruise-ship.test.js
@@ -2,6 +2,16 @@ const Ship = require('../src/index');
 
 describe('constructor', () => {
     it('returns an object', () => {
-        expect(Object).toBeInstanceOf(Object);
+        expect(new Ship).toBeInstanceOf(Object);
+    })
+
+    it('initialises a passengers property as an empty array', () => {
+        titanic = new Ship;
+        expect(titanic.passengers).toStrictEqual([]);
+    })
+
+    it('initialises startingPoint property as 0', () => {
+        titanic = new Ship;
+        expect(titanic.startingPoint).toBe(0);
     })
 })
