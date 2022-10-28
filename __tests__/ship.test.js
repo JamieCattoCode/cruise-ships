f2f6ecd258521b9cc496026b57f6998404f7ac3d
Jamie Catto <jamietcatto@gmail.com>
2022-10-28 11:08:40 +0200

    Refactored Ship test suite to remove repetitions with beforeEach hook.

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -4,23 +4,23 @@ const Itinerary = require('../src/itinerary');
 
 describe('constructor', () => 
 {
-    it('returns an object', () => 
+    beforeEach(() =>
     {
         itinerary1 = new Itinerary;
-        expect(new Ship(itinerary1)).toBeInstanceOf(Object);
+        titanic = new Ship(itinerary1);
+    })
+    it('returns an object', () => 
+    {
+        expect(titanic).toBeInstanceOf(Object);
     })
 
     it('initialises a passengers property as an empty array', () => 
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
         expect(titanic.passengers).toStrictEqual([]);
     })
 
     it('has an itinerary of ports as an array', () =>
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
         for (let i = 0; i < titanic.itinerary.length; i++)
         {
             expect(titanic.itinerary[i]).toBeInstanceOf(Object);
@@ -29,62 +29,49 @@ describe('constructor', () =>
 
     it('initialises startingPort as an object', () => 
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
         expect(titanic.currentPort).toBeInstanceOf(Object);
         expect(titanic.currentPort.name).toBe("Bristol");
     })
 
     it('does not have a previous port', () => 
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
         expect(titanic.previous).toBeFalsy();
     })
 
     it('gets added to port on instantiation', () =>
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
         expect(titanic.currentPort.ships).toContain(titanic);
     })
 })
 
 describe('setSail', () => 
 {
-    it('changes previousPort to the currentPort', () =>
+    beforeEach(() =>
     {
         itinerary1 = new Itinerary;
         titanic = new Ship(itinerary1);
         titanic.setSail();
+    })
+
+    it('changes previousPort to the currentPort', () =>
+    {
         expect(titanic.previousPort).toBe(titanic.itinerary[0]);
     })
 
     it('removes value for currentPort property', () => 
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
-        titanic.setSail();
         expect(titanic.currentPort).toBeFalsy();
     })
 
     it('cannot sail further than its itinerary', () => 
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
-        titanic.setSail();
         titanic.dock();
-
         expect(() => titanic.setSail()).toThrowError('End of itinerary reached.')
     })
 
     it('previous currentPort does not contain the ship', () =>
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
-        titanic.setSail();
         titanic.dock();
-
         expect(titanic.itinerary[0].ships).not.toContain(titanic);
     })
 
@@ -92,22 +79,21 @@ describe('setSail', () =>
 
 describe('dock', () =>
 {
-    it('cannot dock if already docked', () => 
+    beforeEach(() =>
     {
         itinerary1 = new Itinerary;
         titanic = new Ship(itinerary1);
         titanic.setSail();
         titanic.dock();
-
+    })
+    
+    it('cannot dock if already docked', () => 
+    {
         expect(() => titanic.dock()).toThrowError('Can only dock after setting sail.')
     })
 
     it('sets Ship\'s port property to next port', () =>
     {
-        itinerary1 = new Itinerary;
-        titanic = new Ship(itinerary1);
-        titanic.setSail();
-        titanic.dock();
         expect(titanic.currentPort.name).toBe("Galicia");
         expect(titanic.currentPort.ships).toContain(titanic);
     })
